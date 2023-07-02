771c5169656b9abbd5a36b5b0c5a6ac2b05d06d0
JohnnyPontes <jcpontes2@gmail.com>
2023-07-02 17:24:29 -0300

    a

--- anoBissexto.py
+++
@@ -1,7 +0,0 @@
-print('----------------------------------')
-print('Ano Bissexto')
-print('----------------------------------')
-ano = input('Digite o ano: ')
-
-if ano % 4 == 0:
-    print('Esse ano é bissexto.')
--- aulaUdemy2.py
+++
@@ -1,6 +0,0 @@
-x = 10
-
-if x < 0:
-    print('Bom dia')
-if x > 0:
-    print('Boa Tarde')
